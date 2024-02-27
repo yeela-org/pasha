85e6bf4c4680ff82cb81b831e23ab2923a233a78
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-02-27 11:09:10 +0200

    add-reviewer

--- services/http.service.ts
+++ services/http.service.ts
@@ -21,6 +21,7 @@ export class HttpService {
     .pipe(
       finalize(() => {
         //TODO: loading
+        //dsd
       }),
       catchError((err) => {
         return this.handleError(err);
