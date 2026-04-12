5b2bb8d53d86e00cabc11504c2c3cf2c45167a5e
Yeela Lifshitz <52451294+yeelali14@users.noreply.github.com>
2026-04-12 17:59:00 +0300

    Add hello.py with a simple addition function (#453)

---
+++ hello.py
@@ -0,0 +1,4 @@
+  x=1+2                                                                                                                        
+  y =    "hello"                                                                                                               
+  def foo( a,b ):                                                                                                              
+      return a+b 
