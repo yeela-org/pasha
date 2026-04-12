7976789049570b7ba015a5b000c495d7d112a134
Yeela Lifshitz <52451294+yeelali14@users.noreply.github.com>
2026-04-12 17:59:48 +0300

    Refactor foo function and remove whitespace

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
-  x=1+2                                                                                                                        
-  y =    "hello"                                                                                                               
-  def foo( a,b ):                                                                                                              
+def foo(a,b):                                                                                                                
       return a+b 
